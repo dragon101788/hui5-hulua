13e03e2e8735d53e870d2d758c37f9d259dbbecc
dragon <dragon101788@sina.com>
2014-07-10 17:03:39 +0800

    hui 3.3.3 修正updatetimer中段错误的问题

--- tools/updatetime.cpp
+++ tools/updatetime.cpp
@@ -183,8 +183,8 @@ int main(int argc, char * argv[])
 	str += hustr("%02d日 ", timenow->tm_mday);
 	str += hustr("星期%s ", xinqi[timenow->tm_wday].nstr());
 	str += hustr("%02d:%02d ", timenow->tm_hour, timenow->tm_min);
-
-	str += hustr("农历%s%s", ny[n_mon].nstr()-1, nr[n_day].nstr()-1);
+	fprintf(stderr,"农历:%s %s\r\n");
+	str += hustr("农历%s%s", ny[n_mon-1].nstr(), nr[n_day-1].nstr());
 
 	str += "\"";
 	str += "/>";
