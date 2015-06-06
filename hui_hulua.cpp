65e8593dfa6e4dc855be47eba5118248563bd90e
dragon101788 <dragon101788@sina.com>
2015-06-06 21:17:57 +0800

    hui5.0 fix can't find coroutine
    Signed-off-by: dragon101788 <dragon101788@sina.com>

--- hui_hulua.cpp
+++ hui_hulua.cpp
@@ -13,7 +13,9 @@ LUA::LUA()
 {
         printf("LUA\n");
         L = lua_open();
+		luaL_checkversion(L);  /* check that interpreter has correct version */
         luaopen_base(L);
+		luaL_openlibs(L);  /* open standard libraries */
 
         hulua::def(L, "_ALERT", LUA::show_error);
         hulua::def(L, "debug_timer", set_debug_timer);
